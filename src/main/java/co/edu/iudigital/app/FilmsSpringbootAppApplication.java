b7cbeabda44be24ca039466339d87a554d199e1a
jmartinez <jmartinez@cobiscorp.com>
2021-03-30 20:12:28 -0500

    comentario de prueba

--- src/main/java/co/edu/iudigital/app/FilmsSpringbootAppApplication.java
+++ src/main/java/co/edu/iudigital/app/FilmsSpringbootAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmsSpringbootAppApplication {
-
+	// Metodo principal
 	public static void main(String[] args) {
 		SpringApplication.run(FilmsSpringbootAppApplication.class, args);
 	}
