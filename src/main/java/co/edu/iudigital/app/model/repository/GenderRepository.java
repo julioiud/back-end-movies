5d7f4d668df5ed08fb8e440ec50530f52a1993db
jmartinez <jmartinez@cobiscorp.com>
2021-03-30 22:22:05 -0500

    envio del back-end completo

--- src/main/java/co/edu/iudigital/app/model/repository/GenderRepository.java
+++ src/main/java/co/edu/iudigital/app/model/repository/GenderRepository.java
@@ -1,6 +1,5 @@
 package co.edu.iudigital.app.model.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.iudigital.app.model.entity.Gender;
--- src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
+++ src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import co.edu.iudigital.app.model.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	public User findByEmailAndPassword(String email, String password);
+	
 }
